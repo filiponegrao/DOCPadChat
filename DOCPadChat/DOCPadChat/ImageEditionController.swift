4dbc3584d01300545403d2e72647510777e0a489
Verônica <veronica.montezuma@gmail.com>
2016-07-11 18:52:34 -0300

    tela de edição funfano bonitinea

--- DOCPadChat/DOCPadChat/ImageEditionController.swift
+++ DOCPadChat/DOCPadChat/ImageEditionController.swift
@@ -28,8 +28,8 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
 
     var usermodel : UserModel!
     
-    //referentes ao traçado
-    var brushWidth : CGFloat = 2.0
+    // Referentes ao traçado
+    var brushWidth : CGFloat = 4.0
     
     var lastPoint = CGPoint.zero
     
@@ -38,8 +38,9 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
     var mainImageView: UIImageView! //drawing so far
     var tempImageView: UIImageView! //temporary image view for the line currently drawing
     
-    //array de cores
+    var editions = [UIImageView]()
     
+    // Array de cores
     let colors = [color0, color1, color2, color3, color4, color5, color6, color7, color8, color9, color10, color11]
     
     var cor = color0
@@ -69,7 +70,7 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
         self.imageEdit = ImageEditionView(image: self.image, frame: viewFrame, controller: self)
         self.view.addSubview(self.imageEdit)
         
-        //referentes ao traçado
+        // Referentes ao traçado
         mainImageView = UIImageView(frame:self.imageEdit.imageView.bounds)
         
         self.imageEdit.imageView.addSubview(mainImageView)
@@ -78,37 +79,37 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
         self.imageEdit.imageView.addSubview(tempImageView)
 
         
-        //AQUI MÉTODO QUE ESCOLHE ESPESSURA
+        // AQUI MÉTODO QUE ESCOLHE ESPESSURA
         self.thicknessSelected = true //por padrao começa selecionado
         self.pencilSelected = true //por padrao começa selecionado
         self.imageEdit.thicknessButton.addTarget(self, action: #selector(ImageEditionController.chooseThickness), forControlEvents: .TouchUpInside)
         
-        //AQUI MÉTODO QUE ESCOLHE COR
+        // AQUI MÉTODO QUE ESCOLHE COR
         self.imageEdit.colorButton.addTarget(self, action: #selector(ImageEditionController.chooseColor), forControlEvents: .TouchUpInside)
         
-        //AQUI MÉTODO QUE REVERTE UM PASSO
+        // AQUI MÉTODO QUE REVERTE UM PASSO
         self.imageEdit.undoButton.addTarget(self, action: #selector(ImageEditionController.undoAction), forControlEvents: .TouchUpInside)
         
-        //AQUI MÉTODO QUE REMOVE PINTURA
+        // AQUI MÉTODO QUE REMOVE PINTURA
         self.imageEdit.cleanButton.addTarget(self, action: #selector(ImageEditionController.cleanAllPaint), forControlEvents: .TouchUpInside)
         
-        //AÇÃO DO LAPIS
+        // AÇÃO DO LAPIS
         self.imageEdit.pencilButton.addTarget(self, action: #selector(ImageEditionController.pencilAction), forControlEvents: .TouchUpInside)
         
-        //AÇÃO DO PINCEL
+        // AÇÃO DO PINCEL
         self.imageEdit.brushButton.addTarget(self, action: #selector(ImageEditionController.brushAction), forControlEvents: .TouchUpInside)
         
-        //AÇÃO DO MARCADOR
+        // AÇÃO DO MARCADOR
         self.imageEdit.markButton.addTarget(self, action: #selector(ImageEditionController.markAction), forControlEvents: .TouchUpInside)
         
-        //collection delegate
+        // Collection delegate
         self.imageEdit.collection.delegate = self
         self.imageEdit.collection.dataSource = self
-    self.imageEdit.collection.registerClass(ImageEditionCell.self, forCellWithReuseIdentifier: "Cell")
+        self.imageEdit.collection.registerClass(ImageEditionCell.self, forCellWithReuseIdentifier: "Cell")
     }
     
     /*********************************/
-    // ----- testando traçado -----
+    // ----- Traçado de fato -----
     /*********************************/
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -123,63 +124,71 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
     
     func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
         
-        //draws a line between two points in the temporary view
+        // Draws a line between two points in the temporary view
         UIGraphicsBeginImageContext(tempImageView.frame.size)
         let context = UIGraphicsGetCurrentContext()
         tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: tempImageView.frame.width, height: tempImageView.frame.height))
         
-        //gets the current point and draws a line from lastpoint to currentpoint
+        // Gets the current point and draws a line from lastpoint to currentpoint
         CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
         CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
         
-        //parameters for brush size and brush color
+        // Parameters for brush size and brush color
         CGContextSetLineCap(context, CGLineCap.Round)
         CGContextSetLineWidth(context, brushWidth)
         CGContextSetStrokeColorWithColor(context, self.cor.CGColor)
-
         CGContextSetBlendMode(context, CGBlendMode.Normal)
         
-        //actually draws the path
+        // Actually draws the path
         CGContextStrokePath(context)
         
-        //wraps up the drawing context to render the new line into the temporary image view
+        // Wraps up the drawing context to render the new line into the temporary image view
         tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        // sets gesture to true when user moves finger on canvas
+        // Sets gesture to true when user moves finger on canvas
         swiped = true
         if let touch = touches.first {
             let currentPoint = touch.locationInView(tempImageView)
-            //calls method to draw line
+            // Calls method to draw line
             drawLineFrom(lastPoint, toPoint: currentPoint)
-            print("touch")
             
-            //updates the last point so the next touch starts at the right point
+            // Updates the last point so the next touch starts at the right point
             lastPoint = currentPoint
         }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
-        //checks if the user is in the middle of a swipe
+        // Checks if the user is in the middle of a swipe
         if !swiped {
-            //if not, draws a single point at touch location
+            // If not, draws a single point at touch location
             drawLineFrom(lastPoint, toPoint: lastPoint)
         }
+        
+        
+        // Store lines in an array to allow undo function
+        
         
         // Merge tempImageView into mainImageView
         UIGraphicsBeginImageContext(mainImageView.frame.size)
         mainImageView.image?.drawInRect(CGRect(x:0, y:0, width: mainImageView.frame.width, height: mainImageView.frame.height), blendMode: CGBlendMode.Normal, alpha: 1.0)
         tempImageView.image?.drawInRect(CGRect(x:0, y:0, width: mainImageView.frame.width, height: mainImageView.frame.height), blendMode: CGBlendMode.Normal, alpha: 1.0)
-        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        let imageView = UIImageView(frame: CGRect(x:0, y:0, width: mainImageView.frame.width, height: mainImageView.frame.height))
+        imageView.image = image
+        self.editions.append(imageView)
+        self.mainImageView.addSubview(imageView)
+        
         UIGraphicsEndImageContext()
         
         tempImageView.image = nil
     }
-    // fim do traçado
+    
+    // Fim do traçado
 
     override func didReceiveMemoryWarning()
     {
@@ -219,8 +228,6 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
         self.imageEdit.brushStroke.hidden = false
         self.imageEdit.markButton.hidden = false
         self.imageEdit.markStroke.hidden = false
-        
-        //IMPLEMENTAR MÉTODO QUE DEFINE ESPESSURA DO PINCEL
     }
 
     func chooseColor()
@@ -237,20 +244,24 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
         self.imageEdit.brushStroke.hidden = true
         self.imageEdit.markButton.hidden = true
         self.imageEdit.markStroke.hidden = true
-        
-        //IMPLEMENTAR MÉTODO QUE DEFINE COR DA PINTURA
     }
     
     func undoAction()
     {
-        //IMPLEMENTAR MÉTODO QUE RETROCEDE UMA AÇÃO
+        // MÉTODO QUE RETROCEDE UMA AÇÃO
+        if self.editions.count > 0 {
+            self.editions.last?.removeFromSuperview()
+            self.editions.removeLast()
+        }
     }
     
     func cleanAllPaint()
     {
-        //IMPLEMENTAR MÉTODO QUE APAGA PINTURA
-        mainImageView.image = nil
-
+        // MÉTODO QUE APAGA PINTURA
+        for edition in self.editions {
+            edition.removeFromSuperview()
+            self.editions.removeAtIndex(self.editions.indexOf(edition)!)
+        }
     }
     
     func refreshSelectedThickness()
@@ -309,8 +320,8 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
         self.markSelected = false
         self.refreshSelectedThickness()
         
-        brushWidth = CGFloat(2.0)
-        print("2.0")
+        brushWidth = CGFloat(4.0)
+        print("4.0")
         self.refreshSelectedThickness()
     }
     
@@ -321,8 +332,8 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
         self.markSelected = false
         self.refreshSelectedThickness()
         
-        brushWidth = CGFloat(4.0)
-        print("4.0")
+        brushWidth = CGFloat(8.0)
+        print("8.0")
         self.refreshSelectedThickness()
     }
     
@@ -333,8 +344,8 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
         self.brushSelected = false
         self.refreshSelectedThickness()
         
-        brushWidth = CGFloat(8.0)
-        print("8.0")
+        brushWidth = CGFloat(12.0)
+        print("12.0")
         self.refreshSelectedThickness()
     }
     
@@ -354,19 +365,20 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
     {
-        //sets HEX properties to color array elements
+        // Sets HEX properties to color array elements
         cor = colors[indexPath.item]
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
     {
+        
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! ImageEditionCell
         
         let color = self.colors[indexPath.item]
         
         cell.imageView.backgroundColor = color
     
-        
+    
         cell.frame.size = CGSizeMake(screenWidth/6, screenWidth/6)
         
         return cell
@@ -378,7 +390,25 @@ class ImageEditionController: UIViewController, UICollectionViewDelegate, UIColl
     
     func sendImage()
     {
-        ChatApplication.sharedInstance.sendImageMessage(nil, toContact: self.usermodel.id, image: image)
+        // Merge mainImageView into image
+        var finalImage : UIImage
+        
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        image.drawInRect(CGRect(x:0, y:0, width: mainImageView.frame.width, height: mainImageView.frame.height), blendMode: CGBlendMode.Normal, alpha: 1.0)
+        
+        for edition in self.editions {
+            edition.image?.drawInRect(CGRect(x:0, y:0, width: mainImageView.frame.width, height: mainImageView.frame.height), blendMode: CGBlendMode.Normal, alpha: 1.0)
+        }
+        
+//        mainImageView.image?.drawInRect(CGRect(x:0, y:0, width: mainImageView.frame.width, height: mainImageView.frame.height), blendMode: CGBlendMode.Normal, alpha: 1.0)
+        finalImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        mainImageView.image = finalImage
+        
+        
+        // Envia a imagem pro chat
+        ChatApplication.sharedInstance.sendImageMessage(nil, toContact: self.usermodel.id, image: finalImage)
         self.navigationController?.popViewControllerAnimated(true)
         self.chatController.navigationController?.popViewControllerAnimated(true)
     }
